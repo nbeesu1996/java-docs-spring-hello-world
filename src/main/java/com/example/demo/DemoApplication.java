ddc0e89466d6e02e6bae2bf3a319433977725534
nbeesu1996 <121897747+nbeesu1996@users.noreply.github.com>
2023-01-04 14:32:27 +0530

    uri

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/")
+	@RequestMapping("/hi")
 	String sayHello() {
 		return "Hello World! Azure1112";
 	}
